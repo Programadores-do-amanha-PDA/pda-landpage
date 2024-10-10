cc82e6a67acf006822ea22a12fc1345aa5d80e2a
karlla souzza <karlla.souzza7@gmail.com>
2024-10-10 15:30:23 -0300

    feat: adding a empty state on no talents to hire

--- modules/hire-a-talent/Home.tsx
+++ modules/hire-a-talent/Home.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Column, Header } from "@/common/components";
+import { Column, Header, Image, Text } from "@/common/components";
 import StudentsCards from "./StudentsCardsContainer";
 import { Student } from "@/app/api/student/_studentModel";
 import { useEffect, useState } from "react";
@@ -7,22 +7,41 @@ import { getStudents } from "@/hooks/getStudents";
 
 const Home = () => {
   const [students, setStudents] = useState<Student[]>([]);
+  const [loading, setLoading] = useState<boolean | null>(null);
 
   useEffect(() => {
-    getStudents("/student").then((response) => {
-      setStudents(response);
-    });
-  }, []);
+    const fetchStudents = async () => {
+      setLoading(true);
+      const data = await getStudents("/student");
+      setStudents(data);
+      setLoading(false);
+    };
 
-  if (!students) {
-    return <div>Loading...</div>;
-  }
+    fetchStudents();
+  }, []);
 
   return (
     <Column className="flex w-full h-screen items-center justify-center md:justify-start bg-gray-50 dark:bg-secondary-900 relative">
       <Header />
 
-      <StudentsCards students={students} />
+      {students.length === 0 && !loading && loading !== null ? (
+        <Column className="w-full h-full justify-center gap-4 items-center">
+          <Image
+            width={200}
+            height={200}
+            src="/assets/empty_states/noTalents.svg"
+            alt=""
+            className="size-72 md:size-96"
+          />
+          <Text className="font-bold text-lg text-center">
+            Nenhum talento foi encontrado no momento.
+            <br />
+            Verifique novamente mais tarde!
+          </Text>
+        </Column>
+      ) : (
+        <StudentsCards students={students} loading={loading} />
+      )}
     </Column>
   );
 };
