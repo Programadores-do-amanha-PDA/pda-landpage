bc647e085543c209d16d3e2cde710c2e30b7abbd
Karlla Souzza <karlla.souzza7@gmail.com>
2025-11-24 07:58:29 -0300

    refactor: Await `requestLocale` for i18n configuration.

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,6 +1,7 @@
 import { getRequestConfig } from "next-intl/server";
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale;
   return {
     messages: (await import(`./messages/${locale}.json`)).default,
   };
